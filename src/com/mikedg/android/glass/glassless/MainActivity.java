03105e0f5cf8f7240109ace41517f64ce895dca0
mdigiovanni <vinblau@gmail.com>
2013-10-03 07:26:30 -0400

    Added sugar glider as a single activity. Added some test menu items.

--- src/com/mikedg/android/glass/glassless/MainActivity.java
+++ src/com/mikedg/android/glass/glassless/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mikedg.android.glass.glassless;
 
+import java.util.List;
+
 import com.google.glass.samples.compass.CompassActivity;
 import com.google.glass.samples.level.LevelActivity;
 import com.google.glass.samples.stopwatch.StopWatchActivity;
@@ -9,9 +11,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.LayoutInflater.Factory;
@@ -20,12 +27,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnInitListener {
+
+	private TextToSpeech mTts;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+        mTts = new TextToSpeech(this, this);
+        
+        readAllSensors();
+	}
+
+	private void readAllSensors() {
+		SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+		for (Sensor sensor: deviceSensors) {
+			Log.d("Glassless", sensor.getName());
+		}
 	}
 
 	@Override
@@ -46,6 +67,16 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
+	public void onOptionsMenuClosed(Menu menu) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				openOptionsMenu();
+			}
+		});
+	}
+	
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.waveform_menu_item:
@@ -60,11 +91,24 @@ public class MainActivity extends Activity {
 			case R.id.compass_menu_item:
 				startActivity(new Intent(this, CompassActivity.class));
 				break;
+			case R.id.sugar_menu_item:
+				startActivity(new Intent(this, com.mimming.sugarglider.MainActivity.class));
+				break;
+			case R.id.tts_menu_item:
+				testTts();
+				break;
 			default:
 				return super.onOptionsItemSelected(item);
 		}
 		return true;
 	}
 
+	public void testTts() {
+		mTts.speak("Hi there", TextToSpeech.QUEUE_FLUSH, null);
+		mTts.speak("Testing TTS", TextToSpeech.QUEUE_ADD, null);
+	}
 	
+	@Override
+	public void onInit(int status) {
+	}
 }
---
+++ src/com/mimming/sugarglider/MainActivity.java
@@ -0,0 +1,119 @@
+package com.mimming.sugarglider;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.mikedg.android.glass.glassless.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    protected static final String TAG = "sugarglider";
+	private LocationManager locationManager;
+	private ImageView floatingMap;
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_sugar);
+        
+        floatingMap = (ImageView) findViewById(R.id.imageView1);
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    }
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		locationManager.removeUpdates(locationListener);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+	    List<String> providers = locationManager.getAllProviders();
+	    for (String provider : providers) {
+	        if (locationManager.isProviderEnabled(provider)) {
+	            locationManager.requestLocationUpdates(provider, 5000, 10, locationListener);
+	        }
+	    }		
+	}
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location loc) {
+            Log.v(TAG, "Location manager detected a change");
+            String url = "http://maps.googleapis.com/maps/api/staticmap?center=" +
+                    loc.getLatitude() + "," + loc.getLongitude() +
+                    "&zoom=17&size=630x350&maptype=roadmap&markers=color:green%7Clabel:G%7C" +
+                    loc.getLatitude() + "," + loc.getLongitude() +
+                    "&sensor=false&key=AIzaSyB86OWhdiF64GeNbugDTr_xDK3ezrHWlI8" +
+                    "&style=feature:road.local%7Celement:geometry%7Ccolor:0x009900%7Cweight:1%7Cvisibility:on&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0x000000%7Cvisibility:on&style=feature:administrative%7Celement:labels%7Cweight:3.9%7Cvisibility:on%7Cinvert_lightness:true&style=feature:poi%7Cvisibility:simplified";
+            new ImageFetcher().execute(url);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+    };
+
+    private class ImageFetcher extends AsyncTask<String, Void, Drawable> {
+
+        private Exception exception;
+
+        @Override
+        protected Drawable doInBackground(String... urls) {
+            try {
+                String url = urls[0];
+//                InputStream is = (InputStream) new URL("http://mimming.com/index_assets/headshot_logo.jpg").getContent();
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet request = new HttpGet(url);
+                HttpResponse response = httpclient.execute(request);
+
+                InputStream is = response.getEntity().getContent();
+
+//                HttpURLConnection connection = (HttpURLConnection)new URL(urls[0]).openConnection();
+//                InputStream is = connection.getInputStream();
+
+                return Drawable.createFromStream(is, "src name");
+            } catch (Exception e) {
+                Log.v(TAG, "Failed to load image: " + e.getMessage());
+                this.exception = e;
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Drawable drawable) {
+            Log.v(TAG, "Got image, drawoing map");
+
+            floatingMap.setImageDrawable(drawable);
+            floatingMap.setMinimumHeight(360);
+            floatingMap.setMinimumWidth(640);
+        }
+    }
+
+}
